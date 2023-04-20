005a233f6eb88f1e69db0a81bd1561d352098577
laupierre <laupierre@users.noreply.github.com>
2023-04-20 11:59:40 +0200

    Update enrichment_analysis.R

--- enrichment_analysis.R
+++ enrichment_analysis.R
@@ -1,18 +1,66 @@
 library (openxlsx)
 library (gprofiler2)
 
-## The enrichment analysis takes only significantly differentially expressed genes
+
+## The enrichment analysis takes only the enrichment of significantly differentially expressed genes
 
 # 3 months GLONG mouse
 res <- read.xlsx ("GLONG_3months_vs_WT_2023.xlsx")
 
-res <- res[res$padj <= 0.05, ]
-genes.up <- gsub ("\\..*", "", res$Geneid[res$log2FoldChange >0]
-genes.down <- res$Geneid[res$log2FoldChange <0]
+# Take all expressed genes for the background
+all_genes <- gsub ("\\..*", "", res$Geneid)
 
-gp_up <- gost(query = genes.up, organism = "mmusculus", significant= FALSE, numeric_ns = "ENTREZGENE_ACC", sources = c("GO:BP"), exclude_iea=TRUE, evcodes = TRUE)
+# keep only the significant genes
+results_sig <- subset(res, padj < 0.05)
+# get the significant up-regulated genes
+up <- subset(results_sig, log2FoldChange > 0)
+# get the significant down-regulated genes
+down <- subset(results_sig, log2FoldChange < 0)
+
+
+## Accounting for the order of genes in the enrichment analysis
+## See https://f1000research.com/articles/9-709
+
+# order genes by log2FC (from the most up-regulated to the least up-regulated)
+up_ordered <- up[order(up$log2FoldChange, decreasing = TRUE),]
+genes.up <- gsub ("\\..*", "", up_ordered$Geneid)
+
+# order genes by log2FC (from the most down-regulated to the least down-regulated)
+down_ordered <- down[order(down$log2FoldChange, decreasing = FALSE),]
+genes.down <- gsub ("\\..*", "", down_ordered$Geneid)
+
+
+## Up analysis
+gp_up <- gost(query = genes.up, organism = "mmusculus", ordered_query = TRUE, significant= TRUE, numeric_ns = "ENTREZGENE_ACC", sources = c("GO:BP"), 
+              exclude_iea=TRUE, evcodes = TRUE, correction_method = "gSCS", custom_bg = all_genes)
+              
 # remove the evcodes and the intersections
 gp_up <- gp_up$result[ ,-15]
 gp_up <- gp_up[ ,-15]
+# collect the results
+gp_up <- gp_up[ ,c("term_id", "term_name", "p_value", "significant", "intersection_size", "term_size")]
 head(gp_up)
+write.xlsx (gp_up, "GLONG_3months_vs_WT_2023_goa_up_genes.xlsx", rowNames=F)
+
+
+
+## Down analysis
+gp_down <- gost(query = genes.down, organism = "mmusculus", ordered_query = TRUE, significant= TRUE, numeric_ns = "ENTREZGENE_ACC", sources = c("GO:BP"), 
+              exclude_iea=TRUE, evcodes = TRUE, correction_method = "gSCS", custom_bg = all_genes)
+              
+# remove the evcodes and the intersections
+gp_down <- gp_down$result[ ,-15]
+gp_down <- gp_down[ ,-15]
+# collect the results
+gp_down <- gp_down[ ,c("term_id", "term_name", "p_value", "significant", "intersection_size", "term_size")]
+head(gp_down)
+write.xlsx (gp_down, "GLONG_3months_vs_WT_2023_goa_down_genes.xlsx", rowNames=F)
+
+
+
+
+
+
+
+
 
